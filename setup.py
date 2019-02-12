e090db772f21bd2d2154fc4a93440d7713a3ded5
Geoffrey Koh <geoffkoh@yahoo.com.sg>
2019-02-13 00:09:23 +0800

    More changes

--- setup.py
+++ setup.py
@@ -14,13 +14,11 @@ from pkg_resources import require, VersionConflict
 from setuptools import setup, find_packages
 
 try:
-    require('setuptools>=38.3')
+    require('setuptools>=38.2.4')
 except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
+    print("Error: version of setuptools is too old (<38.2.4)!")
     sys.exit(1)
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True,
-          packages=find_packages(exclude=['tests', 'docs', 'dist', 'build']),
-          entry_points={})
+    setup(packages=find_packages(exclude=['tests', 'docs', 'dist', 'build']))
